2583395b9118ba76312679f59fe87352d6cda8fe
archmandan <dan.thedev@outlook.com>
2024-12-15 17:38:19 +0000

    General updates

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -19,20 +19,19 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      local lspconfig = require("lspconfig") 
-      
+      local lspconfig = require("lspconfig")
+
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
-      
 
-
-
-      
       lspconfig.csharp_ls.setup({
         capabilities = capabilities
       })
 
+      lspconfig.ast_grep.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
